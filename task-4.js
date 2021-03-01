e14f660235db34865f5eed67f3cba6b39138ab0f
VladGumeniuk <76571048+VladGumeniuk@users.noreply.github.com>
2021-03-01 16:03:21 +0200

    task-4update

--- task-4.js
+++ task-4.js
@@ -1,29 +1,20 @@
 "use strict";
 
-// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
-
-// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
-// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
-// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
-
-const button = {
-  sub: document.querySelector("[data-action='increment']"),
-  add: document.querySelector("[data-action='decrement']"),
-  span: document.querySelector("#value"),
-};
-
 let counterValue = 0;
-const increment = () => {
-  counterValue += 1;
+let checkValue = document.querySelector("#value");
 
- button.span.textContent = counterValue;
-};
+function increment() {
+  counterValue = counterValue + 1;
+  checkValue.textContent = counterValue;
+}
 
-const decrement = () => {
-  counterValue -= 1;
+function decrement() {
+  counterValue = counterValue - 1;
+  checkValue.textContent = counterValue;
+}
 
-  button.span.textContent = counterValue;
-};
+const firstBtn = document.querySelector(' button[data-action="decrement"]');
+const secondBtn = document.querySelector(' button[data-action="increment"]');
 
-button.sub.addEventListener("click", increment);
-button.add.addEventListener("click", decrement);
+firstBtn.addEventListener("click", decrement);
+secondBtn.addEventListener("click", increment);
