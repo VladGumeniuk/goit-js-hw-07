35be8212e72d18e5712b4b97100af495b3d8ec8d
VladGumeniuk <76571048+VladGumeniuk@users.noreply.github.com>
2021-03-01 19:22:48 +0200

    task-7new

--- task-6.js
+++ task-6.js
@@ -1,16 +1,20 @@
 "use strict";
 
-const checkInput = document.querySelector("#validation-input");
-checkInput.addEventListener("blur", writeCorrectQtySymbol);
+let inputVal = document.getElementById("validation-input");
 
-function writeCorrectQtySymbol(event) {
-  if (
-    checkInput.value.length === Number(checkInput.getAttribute("data-length"))
-  ) {
-    checkInput.classList.remove("invalid");
-    checkInput.classList.add("valid");
-  } else {
-    checkInput.classList.add("invalid");
-    checkInput.classList.remove("valid");
+let totalLenght = inputVal.getAttribute("data-length");
+let intTotallenght = parseInt(totalLenght, 10);
+
+inputVal.oninput = function() {
+  if (inputVal.value.length === intTotallenght) {
+    inputVal.classList.remove("invalid");
+    inputVal.classList.add("valid");
   }
-}
+  if (inputVal.value.length === 0) {
+    inputVal.classList.remove("valid");
+    inputVal.classList.remove("invalid");
+  }
+  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+    inputVal.classList.add("invalid");
+  }
+};
